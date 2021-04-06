b88572e2be95020195316f9877e4eed0df035a03
x-sonny-x <dbstjr5517@naver.com>
2021-04-06 22:39:14 +0900

    Start Project

---
+++ src/main/java/spring/useAws/UseAwsApplication.java
@@ -0,0 +1,13 @@
+package spring.useAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseAwsApplication.class, args);
+	}
+
+}
