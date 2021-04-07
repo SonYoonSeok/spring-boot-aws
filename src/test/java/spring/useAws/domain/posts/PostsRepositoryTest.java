806612fb3668b05c511ff9cff46ed47e19495f54
x-sonny-x <dbstjr5517@naver.com>
2021-04-07 16:20:33 +0900

    Post Repository Test

--- src/test/java/spring/useAws/domain/posts/PostsRepositoryTest.java
+++ src/test/java/spring/useAws/domain/posts/PostsRepositoryTest.java
@@ -2,15 +2,19 @@ package spring.useAws.domain.posts;
 
 import org.junit.After;
 import org.junit.Test;
+
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PostsRepositoryTest {
+public class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
@@ -39,6 +43,6 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
-    }
 
+    }
 }
