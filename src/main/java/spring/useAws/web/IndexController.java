1e20652a1464fdb4c57eecba6013d609567ac1b1
x-sonny-x <dbstjr5517@naver.com>
2021-04-13 19:06:46 +0900

    ADD OAUTH 2.0

--- src/main/java/spring/useAws/web/IndexController.java
+++ src/main/java/spring/useAws/web/IndexController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import spring.useAws.service.posts.PostsService;
+import spring.useAws.web.Dto.PostsResponceDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponceDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
